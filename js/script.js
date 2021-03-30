29fbbdc77892f3d7fc352a78d5d31130c808be40
Greg <maza.gregory46@gmail.com>
2021-03-30 11:34:07 +0200

    feat: add dropdown Menu fixes #11

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
+
+    /*Quand l'utilisateur clique sur le bouton Menu
+basculer entre masquer et afficher le contenu de la liste déroulante  */
+    function displayMenu() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+    //Ferme le dropdown Menu si l'utilisateur clique en dehors
+    //document.getElementsByClassName("dropbtn").onclick(myFunction());
+    $('.dropbtn').on("click", displayMenu);
+
+
     $.ajax({
             //L'URL de la requête 
             url: "https://www.breakingbadapi.com/api/characters",
@@ -16,32 +28,32 @@ $(document).ready(function() {
             //Déclaration d'une const du nom de content pour y mettre une DIV vierge
             const content = document.createElement('div');
             console.log(response);
-                    response.forEach(element => {
-                        const content = document.createElement('div');
-                        //création des balises paragraph et img
-                        const paragraph = document.createElement('p');
-                        const paragraph2 = document.createElement('p');
-                        //const paragraph3 = document.createElement('p');
-                        const image = document.createElement('img');
-                                    
-                        //injecter les données de l'API dans les balises 
-                        paragraph.innerHTML = /*"<p>Pseudo : </p>" + */element.nickname;
-                        paragraph2.innerHTML = /*"<p>name </p>" + */element.name;
-                        //paragraph3.innerHTML = /*"<p>Birthday </p>" + */element.birthday;
-                        image.src = element.img;
-                                    
-                        /*paragraph.style = 'color: red'*/ // ajouter du style CSS
-                        paragraph.style = 'font-size: 1em';
-                        
-                        //Content = DIV (pour l'exemple et appenchild va mettre les balise injecté des données de l'api
-                        content.appendChild(paragraph);
-                        content.appendChild(paragraph2);
-                        //content.appendChild(paragraph3);
-                        content.appendChild(image);
-                        //Modifier le DOM ( document)  querySelector est le selecteur de la balise "API2"
-                        const API2 = document.querySelector(".API2")
-                        console.log(API2);
-                        API2.appendChild(content);
+            response.forEach(element => {
+                const content = document.createElement('div');
+                //création des balises paragraph et img
+                const paragraph = document.createElement('p');
+                const paragraph2 = document.createElement('p');
+                //const paragraph3 = document.createElement('p');
+                const image = document.createElement('img');
+
+                //injecter les données de l'API dans les balises 
+                paragraph.innerHTML = /*"<p>Pseudo : </p>" + */ element.nickname;
+                paragraph2.innerHTML = /*"<p>name </p>" + */ element.name;
+                //paragraph3.innerHTML = /*"<p>Birthday </p>" + */element.birthday;
+                image.src = element.img;
+
+                /*paragraph.style = 'color: red'*/ // ajouter du style CSS
+                paragraph.style = 'font-size: 1em';
+
+                //Content = DIV (pour l'exemple et appenchild va mettre les balise injecté des données de l'api
+                content.appendChild(paragraph);
+                content.appendChild(paragraph2);
+                //content.appendChild(paragraph3);
+                content.appendChild(image);
+                //Modifier le DOM ( document)  querySelector est le selecteur de la balise "API2"
+                const API2 = document.querySelector(".API2")
+                console.log(API2);
+                API2.appendChild(content);
             });
         })
 
@@ -55,7 +67,8 @@ $(document).ready(function() {
     .always(function() {
         alert("Requête effectuée.... Veuillez Patienter un instant");
     });
-    
+
+
+
+
 });
-
-
