5237b0ae9f9249756b2ea8e33cf82335f66c2b40
Greg <maza.gregory46@gmail.com>
2021-03-31 16:44:46 +0200

    feat : update files

--- js/script.js
+++ js/script.js
@@ -51,8 +51,8 @@ basculer entre masquer et afficher le contenu de la liste déroulante  */
                 //content.appendChild(paragraph3);
                 content.appendChild(image);
                 //Modifier le DOM ( document)  querySelector est le selecteur de la balise "API2"
-                const API2 = document.querySelector(".API2")
-                    //console.log(API2);
+                const API2 = document.querySelector(".API2");
+                //console.log(API2);
                 API2.appendChild(content); //Injecter content dans la DIV API2
             });
         })
